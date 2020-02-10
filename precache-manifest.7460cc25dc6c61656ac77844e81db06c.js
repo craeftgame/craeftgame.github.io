88ff4e697551e95b0848e5716d86643c5c3983eb
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-02-10 09:31:57 +0100

    Updates

---
+++ precache-manifest.7460cc25dc6c61656ac77844e81db06c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c171ec2c941b108ba9ec1548dfb1497a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e9699e8b70289ad149",
+    "url": "/static/css/2.1d10ae30.chunk.css"
+  },
+  {
+    "revision": "8d7d78ba187f695d911a",
+    "url": "/static/css/main.881f85d1.chunk.css"
+  },
+  {
+    "revision": "d4e9699e8b70289ad149",
+    "url": "/static/js/2.3b02940c.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/static/js/2.3b02940c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7d78ba187f695d911a",
+    "url": "/static/js/main.67db4aed.chunk.js"
+  },
+  {
+    "revision": "5be0fa323577bdd572d6",
+    "url": "/static/js/runtime-main.38597ccb.js"
+  },
+  {
+    "revision": "9c9bb44e93b2f7bbc7c460d8da296074",
+    "url": "/static/media/filters.9c9bb44e.svg"
+  },
+  {
+    "revision": "a9aa6ccfeeb8c16b68dc5d177b81eb8c",
+    "url": "/static/media/merchant.a9aa6ccf.jpg"
+  },
+  {
+    "revision": "a19e80e23035bd8cef64e3e881339b01",
+    "url": "/static/media/upc.a19e80e2.png"
+  },
+  {
+    "revision": "f65bc98ebe5456a9a03e9cafe781c49f",
+    "url": "/static/media/wf.f65bc98e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1957f92e6620040d56c7c624a8e6ca28.js"
+  "/precache-manifest.7460cc25dc6c61656ac77844e81db06c.js"
 );
 
 self.addEventListener('message', (event) => {
